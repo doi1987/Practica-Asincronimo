f7d4ae1a456c7d8b4f2772b48940a548ce052be5
David Ortega <davidortegaiglesias@gmail.com>
2024-06-24 19:22:54 +0200

    Init project

---
+++ PracticaIOSAsincronismo/PracticaIOSAsincronismo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaIOSAsincronismo
+//
+//  Created by David Ortega Iglesias on 24/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
